9ce35b1ba0d6eb0f71d45c8eebe5ba5cef1d5697
tedski999 <ski@h8c.de>
2022-09-09 19:46:33 +0100

    Initial commit

---
+++ init.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+hash sudo yay || {
+	2>&1 echo "Error: Please install sudo and yay first."
+	exit 1
+}
+
+echo "
+/*
+!/.xinitrc
+
+!/.config/
+!/.config/bspwm/
+!/.config/dunst/
+!/.config/fish/
+!/.config/git/
+!/.config/kitty/
+!/.config/neofetch/
+!/.config/npm/
+!/.config/nvim/
+!/.config/picom/
+!/.config/polybar/
+!/.config/python/
+!/.config/rofi/
+!/.config/sxhkd/
+!/.config/wget/
+!/.config/wget/
+!/.config/zathura/
+!/.config/user-dirs.dirs
+!/.config/user-dirs.locale
+
+!/.local/
+!/.local/bin/
+!/.local/root/
+
+!/.gnupg/
+!/.gnupg/gpg-agent.conf
+!/.gnupg/sshcontrol
+" >> $HOME/.local/dots/info/exclude
+
+yay -Syu fish exa
+# TODO:
+# acpi bat betterdiscord-installer borg brave-bin bspwm
+# btop calc clipmenu didyoumean-bin discord dunst efibootmgr exa
+# fish hsetroot jq kitty lf light mpv neofetch neovim networkmanager
+# noto-fonts noto-fonts-cjk noto-fonts-emoji nvidia nvtop otf-latin-modern
+# pacman-contrib picom pipewire pipewire-alsa pipewire-jack pipewire-pulse
+# polybar pulsemixer rofi rsync socat steam sxhkd terminus-font-ttf
+# ttf-nerd-fonts-symbols-2048-em wireplumber xautolock xclip xorg-xinit
+# xorg-xinput xorg-xset xorg-xsetroot zathura zathura-pdf-mupdf zip
+
+# TODO: symlink root configs
+
+chsh -s /usr/bin/fish
