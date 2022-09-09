6a6d538aa4db8651db09b2ae81256301d3eb718f
tedski999 <ski@h8c.de>
2022-09-09 21:50:32 +0100

    Initial commit

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+echo "
+/*
+!/.config
+!/.gnupg
+
+/.config/*
+!/.config/fish
+!/.config/git
+!/.config/nvim
+
+/.gnupg/*
+!/.gnupg/gpg-agent.conf
+!/.gnupg/sshcontrol
+" > $HOME/.local/dots/info/exclude
+
+apt update
+apt install fish exa gnupg neovim borgbackup rsync
+chsh -s fish
