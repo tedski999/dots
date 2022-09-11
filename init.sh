b5275a3df6bd201cbebee6fc626afd61e7848c14
tedski999 <ski@h8c.de>
2022-09-11 14:41:39 +0100

    Update GPG key

--- init.sh
+++ init.sh
@@ -8,12 +8,22 @@ hash sudo || {
 echo "
 /*
 !/.config
+!/.gnupg
+!/.ssh
+
 /.config/*
 !/.config/fish
 !/.config/git
 !/.config/nvim
+
+/.gnupg/*
+!/.gnupg/gpg-agent.conf
+!/.gnupg/sshcontrol
+
+/.ssh/*
+!/.ssh/authorized_keys
 " > $HOME/.local/dots/info/exclude
 
 sudo apt-get update
-sudo apt-get install fish exa neovim borgbackup rsync
+sudo apt-get install fish exa neovim rsync
 chsh -s /usr/bin/fish
