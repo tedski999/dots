8e01a6aa02942589dcf499be112ead360017aa85
tedski999 <ski@h8c.de>
2023-05-04 19:55:18 +0100

    Replace nvim codewindow with satellite

--- .config/nvim/lua/plugins/codewindow.lua
+++
@@ -1,8 +0,0 @@
--- TODO: replace with scroll bar
-return {
-	"gorbit99/codewindow.nvim",
-	keys = { { "<leader>m", "<cmd>lua require('codewindow').toggle_minimap()<cr>" } },
-	config = function()
-		require("codewindow").setup({ minimap_width = 16, window_border = "" })
-	end
-}
---
+++ .config/nvim/lua/plugins/satellite.lua
@@ -0,0 +1,13 @@
+return {
+	"lewis6991/satellite.nvim",
+	lazy = false,
+	opts = {
+		winblend = 25,
+		handlers = {
+			search = { enable = true },
+			diagnostic = { enable = true },
+			gitsigns = { enable = false },
+			marks = { enable = true }
+		}
+	}
+}
