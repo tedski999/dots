fc4d8da0445bbe84f178b57d65213666e208ce16
tedski999 <ski@h8c.de>
2023-05-07 23:12:24 +0100

    Add ai and cursorword nvim plugins

---
+++ .config/nvim/lua/plugins/cursorword.lua
@@ -0,0 +1,11 @@
+return {
+	"xiyaowong/nvim-cursorword",
+	event = "CursorMoved",
+	config = function()
+		vim.cmd("highlight CursorWord gui=bold")
+		vim.g.cursorword_disable_filetypes = {}
+		vim.g.cursorword_disable_at_startup = false
+		vim.g.cursorword_min_width = 1
+		vim.g.cursorword_max_width = 50
+	end
+}
