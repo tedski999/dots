1c46a4042de524674f89722c4945a7f478c09f69
tedski999 <ski@h8c.de>
2023-05-09 23:04:56 +0100

    Revert lazy loading polyglot

--- .config/nvim/lua/plugins/polyglot.lua
+++ .config/nvim/lua/plugins/polyglot.lua
@@ -1,6 +1,6 @@
 return {
 	"sheerun/vim-polyglot",
-	event = "Syntax",
+	lazy = true,
 	config = function()
 		vim.api.nvim_clear_autocmds({ event = { "BufNewFile", "BufRead" }, pattern = "*.cgi,*.fcgi,*.gyp,*.gypi,*.lmi,*.ptl,*.py,*.py3,*.pyde,*.pyi,*.pyp,*.pyt,*.pyw,*.rpy,*.smk,*.spec,*.tac,*.wsgi,*.xpy,{.,}gclient,{.,}pythonrc,{.,}pythonstartup,DEPS,SConscript,SConstruct,Snakefile,wscript", group = "filetypedetect" })
 		vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = "*.cgi,*.fcgi,*.gyp,*.gypi,*.lmi,*.ptl,*.py,*.py3,*.pyde,*.pyi,*.pyp,*.pyt,*.pyw,*.rpy,*.smk,*.spec,*.wsgi,*.xpy,{.,}gclient,{.,}pythonrc,{.,}pythonstartup,DEPS,SConscript,SConstruct,Snakefile,wscript", group = "filetypedetect", command = "setf python" })
