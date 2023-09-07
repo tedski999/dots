f5a50a3bbab6e4d5c83788236176d46c7fa920ad
tedski999 <ski@h8c.de>
2023-09-07 16:01:12 +0100

    Add nix dots config

---
+++ .bashrc
@@ -0,0 +1,1 @@
+ZDOTDIR="$HOME/.config/zsh" exec "$HOME/.local/bin/nix" shell "$HOME/.config/nix" --command zsh
---
+++ .config/nix/flake.nix
@@ -0,0 +1,38 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "dots";
+        paths = with pkgs; [
+          # desktop environment
+          bspwm
+          sxhkd
+          picom #TODO: better picom?
+          clipmenu
+          rofi
+          dmenu
+          # applications
+          alacritty #TODO: replace alacritty?
+          # shell
+          zsh
+          zsh-autosuggestions
+          zsh-completions
+          # cli tools
+          neovim
+          ripgrep
+          fzf
+          bat
+          fd
+          exa
+          delta
+          btop
+
+          #rust
+        ];
+        extraOutputsToInstall = [ "man" "doc" ];
+      };
+    };
+}
