a959375028f562bb7367a54b9f0f437961ab836f
tedski999 <ski@h8c.de>
2023-10-13 22:17:09 +0100

    Fix opt script paths

--- .local/bin/cht.sh
+++ .local/bin/cht.sh
@@ -5,8 +5,9 @@ export PATH=$(echo "$PATH" | tr ":" "\n" | grep -v "$(dirname -- "$0")" | tr "\n
 hash cht.sh 2>/dev/null && exec cht.sh $@
 
 opt="$HOME/.local/opt/cht.sh"
+bin="$opt/cht.sh"
 src="cht.sh/:cht.sh"
 
-[ -d "$opt" ] || { mkdir -p "$opt" && curl -fsSL "$src" > "$opt/cht.sh" && chmod +x "$opt/cht.sh"; } || { rm -r "$opt"; exit 1; }
+[ -d "$opt" ] || { mkdir -p "$opt" && curl -fL "$src" > "$bin" && chmod +x "$bin"; } || { rm -r "$opt"; exit 1; }
 
-exec "$opt/cht.sh" $@
+exec "$bin" $@
