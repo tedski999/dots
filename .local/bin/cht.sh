9ac9e4cb320508de47d248faf9a7c65c0a1c5e78
tedski999 <ski@h8c.de>
2023-10-24 10:19:57 +0100

    Fix opt bin script pattern

--- .local/bin/cht.sh
+++ .local/bin/cht.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export PATH=$(echo "$PATH" | tr ":" "\n" | grep -v "$(dirname -- "$0")" | tr "\n" ":")
-
-hash cht.sh 2>/dev/null && exec cht.sh $@
+for bin in $(realpath $(which -a cht.sh)); do
+	[ "$(realpath $0)" != "$bin" ] && [ -x "$bin" ] && exec "$bin" "$@"
+done
 
 opt="$HOME/.local/opt/cht.sh"
 bin="$opt/cht.sh"
@@ -10,4 +10,4 @@ src="cht.sh/:cht.sh"
 
 [ -d "$opt" ] || { mkdir -p "$opt" && curl -fL "$src" > "$bin" && chmod +x "$bin"; } || { rm -r "$opt"; exit 1; }
 
-exec "$bin" $@
+exec "$bin" "$@"
