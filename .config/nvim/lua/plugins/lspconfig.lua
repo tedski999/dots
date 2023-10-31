af738df0f81d3a236a1c504e57ba753a6ac74632
tedski999 <ski@h8c.de>
2023-10-31 00:57:46 +0000

    Clean up nvim lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,31 +1,19 @@
 -- Preconfigured native LSP configs
+-- TODO: look into installing LSPs/DAPs/linters/formatters with mason
 return {
 	"neovim/nvim-lspconfig",
-	lazy = false,
-	enabled = false,
+	cmd = { "LspStart", "LspStop", "LspInfo" },
+	keys = { { "<leader>z", function() if next(vim.lsp.buf_get_clients()) == nil then vim.notify("Starting LSP...") vim.cmd("LspStart") else vim.notify("Stopping LSP...") vim.cmd("LspStop") end end } },
 	config = function()
 		local lsp = require("lspconfig")
-		require('lspconfig.ui.windows').default_options.border = vim.g.border_chars
+		require("lspconfig.ui.windows").default_options.border = vim.g.border_chars
 
-		lsp.util.on_setup = lsp.util.add_hook_before(lsp.util.on_setup, function(cfg)
-			cfg.capabilities = require("cmp_nvim_lsp").default_capabilities()
-			cfg.on_attach = function(client, bufnr) end
-		end)
-
-		-- TODO(3, aesthetic): move lsp server settings to a table
 		lsp.clangd.setup({})
 		lsp.pylsp.setup({})
 		lsp.rust_analyzer.setup({
-			cmd = {'rustup', 'run', 'stable', 'rust-analyzer'},
-			settings = { ['rust-analyzer'] = { checkOnSave = { overrideCommand = { 'cargo', 'clippy', '--workspace', '--message-format=json', '--all-targets', '--all-features' } } } }
+			cmd = { "rustup", "run", "stable", "rust-analyzer" },
+			settings = { ["rust-analyzer"] = { checkOnSave = { overrideCommand = { "cargo", "clippy", "--workspace", "--message-format=json", "--all-targets", "--all-features" } } } }
 		})
-
-		-- local servers = { "clangd", "pylsp", "rust_analyzer" }
-		-- for server in servers do
-			-- lsp[server].setup(serveropts[server])
-		-- end
-
-		-- TODO(3, aesthetic): keybinding to reload all lsp servers
 
 		if vim.g.arista then
 			vim.api.nvim_create_user_command("Acdb", "belowright split | exec 'terminal echo \"Generating compile_commands.json...\" && cdbtool --tin '.<q-args>", { nargs = 1 })
--- .zshenv
+++ .zshenv
@@ -14,7 +14,6 @@ export BROWSER="firefox"
 export HISTFILE="$XDG_DATA_HOME/history"
 export ANDROID_HOME="$XDG_DATA_HOME/android"
 export CARGO_HOME="$HOME/.local/opt/cargo"
-export PATH="$CARGO_HOME/bin:$PATH"
 export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 export GOPATH="$XDG_DATA_HOME/go"
@@ -23,10 +22,10 @@ export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/pythonrc"
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/config"
 export RUSTUP_HOME="$HOME/.local/opt/rustup"
 export WINEPREFIX="$XDG_DATA_HOME/wine"
-export ZSH_DATA="$XDG_DATA_HOME/zsh"
 export npm_config_userconfig="$XDG_CONFIG_HOME/npm/npmrc"
 export skip_global_compinit=1
 # TODO: .cert
 # Settings
 export MOZ_ENABLE_WAYLAND=1
 export GAMEMODERUNEXEC="env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"
+source "$HOME/.local/opt/cargo/env"
