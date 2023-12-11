4f7019ab9a6ee7377b413ae4e156b0eef691ad6e
tedski999 <ski@h8c.de>
2023-12-11 00:56:09 +0000

    Update system install scripts

--- .local/root/msung.sh
+++ .local/root/msung.sh
@@ -6,20 +6,20 @@ swap="/dev/yyyy"
 crypt="/dev/zzzz"
 
 # format partitions
-mkfs.fat "$boot" -F 32 -n BOOT
-mkswap "$swap" --label SWAP
-cryptsetup luksFormat "$crypt" --label CRYPT
-cryptsetup open /dev/disk/by-label/CRYPT root
-mkfs.btrfs /dev/mapper/root --label ROOT
-mount /dev/disk/by-label/ROOT /mnt
+mkfs.fat "$boot" -F 32 -n MSUNG_BOOT
+mkswap "$swap" --label MSUNG_SWAP
+cryptsetup luksFormat "$crypt" --label MSUNG_CRYPT
+cryptsetup open /dev/disk/by-label/MSUNG_CRYPT root
+mkfs.btrfs /dev/mapper/root --label MSUNG_ROOT
+mount /dev/disk/by-label/MSUNG_ROOT /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume set-default /mnt/@
 umount /mnt
 
 # install system
-mount /dev/disk/by-label/ROOT /mnt
-mount --mkdir /dev/disk/by-label/BOOT /mnt/boot
-swapon /dev/disk/by-label/SWAP
+mount /dev/disk/by-label/MSUNG_ROOT -o ssd,compress=zstd:1 /mnt
+mount --mkdir /dev/disk/by-label/MSUNG_BOOT /mnt/boot
+swapon /dev/disk/by-label/MSUNG_SWAP
 pacstrap -K /mnt base linux linux-firmware sudo git grub efibootmgr
 genfstab -L /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
